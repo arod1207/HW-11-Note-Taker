c3e33c6f1d41e86c697dac668c89f233857a21a9
Arod1207 <arod1207@me.com>
2020-07-01 01:50:30 -0500

    adding new objects to array

--- server.js
+++ server.js
@@ -1,29 +1,43 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // made a static path to public folder //
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // sent html file notes.html //
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
-})
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// returning notes to front end //
+app.get("/api/notes", function (req, res) {
+  return res.sendFile(path.join(__dirname, "db/db.json"));
+});
 
 // creating api routes //
-app.get('/api/notes', (req, res) => {
-  fs.readFile('db/db.json', (err, data) => {
-    if (err) throw err;
-    let notes = JSON.parse(data)
-    return res.json(notes)
-  })
-})
 
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  console.log(newNote);
+
+  fs.readFile("db/db.json", (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    notes.push(newNote);
+
+    fs.writeFile("db/db.json", JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      console.log("file written");
+    });
+    return res.json(newNote);
+  });
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
