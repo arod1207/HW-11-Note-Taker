c2a61b7314e295f0a2f4d7776f5c69b21bb14e60
Arod1207 <arod1207@me.com>
2020-06-30 23:01:35 -0500

    parsed and reading json file

--- server.js
+++ server.js
@@ -16,5 +16,14 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
 
+// creating api routes //
+app.get('/api/notes', (req, res) => {
+  fs.readFile('db/db.json', (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data)
+    return res.json(notes)
+  })
+})
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
